a9506bf9e9ae7ce1eab4e46b69d6adfaca0090fd
Thomas A <thomas.a@erisium.com>
2020-04-03 16:48:47 +0200

    add HomepageController with example translations

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+
+}
